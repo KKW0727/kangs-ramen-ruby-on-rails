2547813dc055f05c87fdb71a76d0403e10977403
kkw0727 <kkw3401@gmaiil.com>
2022-06-06 06:20:06 +0900

    add: admin, user, home routeを設定

--- kangs-ramen/config/routes.rb
+++ kangs-ramen/config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root "articles#index"
+  #  root "articles#index"
+
+  resources :home
+
+  root 'home#index'
+
 end
