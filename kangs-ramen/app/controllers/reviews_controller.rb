14eb3c81a3e6f0184bf3da56fd25ac2e3330993a
kkw0727 <kkw3401@gmaiil.com>
2022-07-25 16:02:45 +0900

    add: review create, permit_params(strong parameter)

--- kangs-ramen/app/controllers/reviews_controller.rb
+++ kangs-ramen/app/controllers/reviews_controller.rb
@@ -8,4 +8,18 @@ class ReviewsController < ApplicationController
         @review = Review.new
     end
 
+    def create
+        review = Review.new(permit_params)
+        if review.save
+             redirect_to reviews_path
+        else
+             redirect_to new_review_path
+        end
+  
+     end
+
+    private
+    def permit_params
+        params.require(:review).permit(:comment, image: []).merge(user_id: current_user.id)
+    end
 end
