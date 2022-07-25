6950e64eceb52e2d114b3faa94f321167fb11755
kkw0727 <kkw3401@gmaiil.com>
2022-07-25 19:25:07 +0900

    add: ホーム画面に5個のレビューを見せるために作成

--- kangs-ramen/app/controllers/home_controller.rb
+++ kangs-ramen/app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     def index
         @menus = Menu.where(is_publish: true)
         @game_info = GameInfo.find_by(id: 1)
+        @reviews = Review.limit(5).order('id desc')
     end
 end
